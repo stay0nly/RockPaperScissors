b22d3659e519802edc9434f276e1a61cdef0e0c1
stay0nly <derkach.dmt@gmail.com>
2022-04-11 20:21:03 +0800

    Event listener added

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
-const pSelection = prompt('Type Rock, Paper, Scissors', '');
-const playerSelection = pSelection.toLowerCase();
+const userScore = 0;
+const pcScore = 0;
+const userScoreNumber = document.getElementById('human_score');
+const pcScoreNumber = document.getElementById('computer_score');
+const scores = document.querySelector('.scores');
+const result = document.querySelector('.result');
+const rockElement = document.getElementById('rock');
+const paperElement = document.getElementById('paper');
+const scissorsElement = document.getElementById('scissors');
 
-function playRound() {
-    var values = ["rock","paper","scissors"];
-    var computerSelection = values[Math.floor(Math.random() * values.length)];
-  
+function game(userChoice) {
+
+}
+
+//Element click on the webpage
+function playerSelection() {
+rockElement.addEventListener('click', function() {
+    game("rock");
+});
+
+paperElement.addEventListener('click', function() {
+    game("paper");
+});
+
+scissorsElement.addEventListener('click', function() {
+    game("scissors");
+});
+};
+
+
+// Generating computer's choice
+function computerPlay() {
+    const values = ["rock","paper","scissors"];
+    const computerSelection = values[Math.floor(Math.random() * 3)];
+    return computerSelection;
+};
+
+//One round play
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        alert("No winner");
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        alert("Computer wins!")
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        alert("Computer wins!")
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        alert("Computer wins!")
+        alert("DRAW!");
+    } else if (
+        (playerSelection === "paper" && computerSelection === "scissors") 
+        (playerSelection === "rock" && computerSelection === "paper")
+        (playerSelection === "scissors" && computerSelection === "rock")) {
+            alert("PC Wins!");          
     } else {
-        alert("Human wins!")
+        alert("Human Wins!");
     }
-    console.log(computerSelection);
-    console.log(playerSelection);
-}
+   
+};
 
-function game() {
-    for (let i=0; i<5; i++) {
-        
-    }
-}
