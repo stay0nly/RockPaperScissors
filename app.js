c8d7b12b849be26bedf326229c6a3b119ecd52f3
stay0nly <derkach.dmt@gmail.com>
2022-04-12 09:26:17 +0800

    Working game prototype. Score board display.

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
-const userScore = 0;
-const pcScore = 0;
+let userScore = 0;
+let pcScore = 0;
 const userScoreNumber = document.getElementById('human_score');
 const pcScoreNumber = document.getElementById('computer_score');
 const scores = document.querySelector('.scores');
-const result = document.querySelector('.result');
+const result = document.querySelector('.result > p');
 const rockElement = document.getElementById('rock');
 const paperElement = document.getElementById('paper');
 const scissorsElement = document.getElementById('scissors');
 
-function game(userChoice) {
 
+// Generating computer's choice
+function computerPlay() {
+    const values = ["rock","paper","scissors"];
+    const computerSelection = values[Math.floor(Math.random() * 3)];
+    return computerSelection;
 }
 
+// Result functions
+function win(playerSelection, computerSelection) {
+    userScore++;
+    userScoreNumber.innerHTML = userScore;
+    result.innerHTML = playerSelection + " beats " + computerSelection + ". You Win! 😎"
+}
+function lose(playerSelection, computerSelection) {
+    pcScore++;
+    pcScoreNumber.innerHTML = pcScore;
+    result.innerHTML = playerSelection + " beats " + computerSelection + ". You Lost! 😥"
+}
+function draw(playerSelection, computerSelection) {
+    result.innerHTML = "😁 It's a Draw! 😁"
+}
+
+//Game
+function game(playerSelection) {
+    const computerSelection = computerPlay();
+    if (playerSelection === computerSelection) {
+        draw(playerSelection, computerSelection);
+    } else if (
+        (playerSelection === "paper" && computerSelection === "scissors") || 
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "scissors" && computerSelection === "rock")) {
+        lose(playerSelection, computerSelection);          
+    } else {
+        win(playerSelection, computerSelection);
+    }
+}
+
+
 //Element click on the webpage
-function playerSelection() {
+function selection() {
 rockElement.addEventListener('click', function() {
     game("rock");
 });
@@ -27,26 +62,22 @@ scissorsElement.addEventListener('click', function() {
 });
 };
 
+selection();
 
-// Generating computer's choice
-function computerPlay() {
-    const values = ["rock","paper","scissors"];
-    const computerSelection = values[Math.floor(Math.random() * 3)];
-    return computerSelection;
-};
+
 
 //One round play
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        alert("DRAW!");
-    } else if (
-        (playerSelection === "paper" && computerSelection === "scissors") 
-        (playerSelection === "rock" && computerSelection === "paper")
-        (playerSelection === "scissors" && computerSelection === "rock")) {
-            alert("PC Wins!");          
-    } else {
-        alert("Human Wins!");
-    }
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection === computerSelection) {
+//         alert("DRAW!");
+//     } else if (
+//         (playerSelection === "paper" && computerSelection === "scissors") || 
+//         (playerSelection === "rock" && computerSelection === "paper") ||
+//         (playerSelection === "scissors" && computerSelection === "rock")) {
+//             alert("PC Wins!");          
+//     } else {
+//         alert("Human Wins!");
+//     }
    
-};
+// };
 
